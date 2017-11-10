40c56dff951d79b49081b0bb13083a9286911315
Erik Olsvik Dengerud <eriko1306@gmail.com>
2017-11-10 21:35:58 +0100

    noen nye plot

--- Area_Plot.py
+++ Area_Plot.py
@@ -9,3 +9,25 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def Area_Plot():
+
+	with G2('Area.g2') as my_surf:
+		my_obj = my_surf.read()
+
+	
+	x = []
+	y = []
+	z = []
+	for vec in my_obj[0]:
+		x.append(vec[0])
+		y.append(vec[0])
+		z.append(0)
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+	ax.plot_surface(x,y,z)
+	plt.show()
+	plt.figure()
+	plt.plot(y,x)
+	plt.show()
+
+Area_Plot()
