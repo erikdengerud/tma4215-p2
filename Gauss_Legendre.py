9567996065393dfe9992226d061d1c2e6f922c82
Øyvind Auestad <oyvind.auestad@hotmail.com>
2017-11-13 22:31:24 +0100

    skrevet litt mer

--- Gauss_Legendre.py
+++ Gauss_Legendre.py
@@ -4,25 +4,39 @@ import sys
 import numpy as np
 from math import *
 import matplotlib.pyplot as plt
+plt.style.use("ggplot")
 ################################################################################
+ERRORTOL = 1e-15
+EPS = 1e-25
+
+def XML_Extraction(xmlfile):
+    tree = et.parse(xmlfile)
+    root = tree.getroot()
+    f = lambda x : eval(root[0].text)
+    analytical = float(root[1].text)
+    return [ f, analytical ]
 
 
-def XML_Extraction(XMLFILE):
-    return
 
 def Gauss_Legendre_Data(n):
-    return
+    # find the n roots of L_n(x)
+    guess = [ np.cos(4 * i - 1) * np.pi / (2 * n + 1) for i in range(1, n + 1) ]
+    xi = [ Olver(n, x_0)[0] for x_0 in guess ]
+    # compute the weights
+    weights = [ 2 / (( 1 - x**2 ) * ( (Legendre_1(n, x, Legendre_0(n, x))[-1])**2 ) ) for x in xi ]
+    return [xi, weights]
 
 def Olver(n,x0):
     x = x0        
     xvalues = [x]           
     errors = []
     for i in range(1000):
-        L_x = Legendre_0(n, x)[-1]
-        L1_x = Legendre_1(n, x)[-1]
-        L2_x = Legendre_2(n, x)[-1]
+        L0_x = Legendre_0(n, x)
+        L1_x = Legendre_1(n, x, L0_x)
+        L2_x = Legendre_2(n, x, L1_x)
         
-        x = x - L_x / L1_x - L2_x * L_x**2 / (2 * L1_x**3)
+        assert(np.abs(L1_x[-1]) > EPS)
+        x = x - L0_x[-1] / L1_x[-1] - L2_x[-1] * L0_x[-1]**2 / (2 * L1_x[-1]**3)
         current_error = np.abs(xvalues[-1] - x)
         errors.append(current_error)
         xvalues.append(x)
@@ -34,51 +48,62 @@ def Olver(n,x0):
 def Legendre_0(n,x):
     if n == 0:
         return [1]
+    if x == -1:
+        return [(-1)**i for i in range(n + 1)]
+    if x == 1:
+        return [1] * (n + 1)
     # result = [ L_0(x), L_1(x), ..., L_n(x) ]
     result = [1, x]
     for i in range (2, n + 1):
         result.append( ((2 * n + 1) * x * result[i - 1] - n * result[i - 2] ) / (n + 1) )
     return result
 
-def Legendre_1(n,x):
+def Legendre_1(n,x, L0):
     if n == 0:
         return [0]
     # result = [ L_0'(x), L_1'(x), ..., L_n'(x) ]
     result = [0, 1]
-    # we need the values of Legendre_0(n, x)
-    L0 = Legendre_0(n, x)
     for i in range (2, n + 1):
         result.append( ( (2 * n + 1) * (L0[i - 1] + x * result[i - 1]) - n * result[i - 2] ) / (n + 1) )
     return result
 
-def Legendre_2(n,x,L0,L1):
+def Legendre_2(n,x, L1):
     if n == 0:
         return [0]
     # result = [ L_0''(x), ..., L_n''(x) ]
     result = [0, 0]
-    # we need the values of L1
-    L1 = Legendre_1(n, x)
     for i in range (2, n + 1):
         result.append( ( (2 * n + 1) * (2 * L1[i - 1] + x * result[i - 1]) - n * result[i - 2] ) / (n + 1) )
     return result
     
+
+
 def Gauss_Legendre_Quadrature(n,G,f):
-    return
+    data = Gauss_Legendre_Data(n)
+    result = np.dot(np.array(data[1]), f(np.array(data[0])))
+    return result
 
-def Return_Quadrature(XMLFILE,n):
-    return
+def Return_Quadrature(xmlfile, n):
+    G = 1 # ?
+    data = XML_Extraction(xmlfile)
+    numeric = Gauss_Legendre_Quadrature(n, G, data[0])
+    analytic = data[1]
     
-xvalues = [-1 + 2 * i / 100 for i in range(101)]
-yvalues = [ Legendre_0(10, x)[-1] for x in xvalues ]
-plt.plot(xvalues, yvalues)
+    return [numeric, analytic]
 
-    
-    
-    
-    
-    
-    
-    
-    
+
+"""
+plt.figure()
+xvalues = [-1 + 2 * i / 1000 for i in range(1001)]
+for j in range(10):
+    yvalues = [Legendre_0(j, x)[-1] for x in xvalues]
+    plt.plot(xvalues, yvalues)
+"""
+"""
+errors = Olver(10, 1)[1]
+plt.semilogy([i for i in range(len(errors))], errors)
+"""
+f = lambda x : 2 * x**3 + 3 * x**2 
+print(Gauss_Legendre_Quadrature(10, 1, f))
     
     
