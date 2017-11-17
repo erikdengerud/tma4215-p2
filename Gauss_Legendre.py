bae28a483b3acf2ae6c09851ef957efe78d49892
Øyvind Auestad <oyvind.auestad@hotmail.com>
2017-11-17 13:51:30 +0100

    symmetri av nullpunkt

--- Gauss_Legendre.py
+++ Gauss_Legendre.py
@@ -6,7 +6,7 @@ from math import *
 import matplotlib.pyplot as plt
 plt.style.use("ggplot")
 ################################################################################
-ERRORTOL = 1e-6
+ERRORTOL = 1e-14
 EPS = 1e-25
 ITERATIONCAP = 2000
 
@@ -15,16 +15,20 @@ def XML_Extraction(xmlfile):
     root = tree.getroot()
     f = lambda x : eval(root[0].text)
     analytical = float(root[1].text)
-    return [ f, analytical ]
-
+    return [f, analytical]
 
 
 def Gauss_Legendre_Data(n):
-    # find the n roots of L_{n}(x)
-    guess = [ ( np.cos((2 * i - 1) * np.pi / (2 * n + 1)) + np.cos(2 * i * np.pi / (2 * n + 1)) ) / 2 for i in range(1, n + 1) ]
+    # find the n roots of L_{n}(x). Since the roots are symmetric about x = 0,
+    # we only need to calculate the first ceil(n / 2) roots in [0, 1]
+    guess = [ ( np.cos((2 * i - 1) * np.pi / (2 * n + 1)) + np.cos(2 * i * np.pi / (2 * n + 1)) ) / 2 for i in range(1, int(np.floor(n / 2)) + 1) ]
+    if n % 2 != 0:
+        guess.append(0)
+    xi = [ Olver(n, x_0) for x_0 in guess ]
+    # add the roots on [-1, 0)
+    for i in range( int(np.floor(n / 2)) - 1, -1, -1 ):
+        xi.append(-xi[i]);
     
-    # print(guess)
-    xi = [ Olver(n, x_0)[0] for x_0 in guess ]
     # compute the weights
     weights = []
     for x in xi:    
@@ -35,7 +39,7 @@ def Gauss_Legendre_Data(n):
 def Olver(n, x0):
     x = x0        
     xvalues = [x]           
-    errors = []
+    # errors = []
     for i in range(ITERATIONCAP):
         L0_x = Legendre_0(n, x)
         L1_x = Legendre_1(n, x, L0_x)
@@ -44,12 +48,11 @@ def Olver(n, x0):
         assert(np.abs(L1_x[-1]) > EPS)
         x = x - L0_x[-1] / L1_x[-1] - L2_x[-1] * L0_x[-1]**2 / (2 * L1_x[-1]**3)
         current_error = np.abs(xvalues[-1] - x)
-        errors.append(current_error)
+        # errors.append(current_error)
         xvalues.append(x)
         if (current_error < ERRORTOL):
-            return x, errors
-        
-    assert(errors[-1] < ERRORTOL)
+            return x #, errors
+    assert(False)
 
 def Legendre_0(n, x):
     if n == 0:
@@ -92,7 +95,7 @@ def Gauss_Legendre_Quadrature(n, G, f):
 def Return_Quadrature(xmlfile, n):
     data = XML_Extraction(xmlfile)
     # n x 2 matrix with nodes and weights
-    # with the n + 1 points and weights we can integrate all polynomials of degree <= 2 * n - 1 exactly
+    # with the n points and weights we can integrate all polynomials of degree <= 2 * n - 1 exactly
     G = Gauss_Legendre_Data(n) 
     numeric = Gauss_Legendre_Quadrature(n, G, data[0])
     analytic = data[1]
@@ -100,7 +103,8 @@ def Return_Quadrature(xmlfile, n):
     return [numeric, analytic]
 
 f = lambda x : 7 * x**12 + 2 * x**3 - 12 * x
+n = 7
+G = Gauss_Legendre_Data(n)
+print( Gauss_Legendre_Quadrature(n, G, f) )
 
-G = Gauss_Legendre_Data(5)
-print( Gauss_Legendre_Quadrature(5, G, f) )
     
