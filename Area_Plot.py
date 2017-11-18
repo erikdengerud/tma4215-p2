702b734a666d4e5d06830863371190e923142ac1
Erik Olsvik Dengerud <eriko1306@gmail.com>
2017-11-18 14:16:17 +0100

    minor

--- Area_Plot.py
+++ Area_Plot.py
@@ -1,7 +1,7 @@
 ################################################################################
 import numpy as np
 import splipy as spl
-from splipy.io import *
+from splipy.io import G2
 import splipy.surface_factory as spf
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -9,25 +9,21 @@ from mpl_toolkits.mplot3d import Axes3D
 
 
 def Area_Plot():
+    with G2("Area.g2") as file:
+        surface = file.read()[0]
 
-	with G2('Area.g2') as my_surf:
-		my_obj = my_surf.read()
+        #2D        
+        N = 30
+        u = np.linspace(surface.start('u'), surface.end('u'), N)
+        v = np.linspace(surface.start('v'), surface.end('v'), N)
+        X = surface(u, v)
+        #Why did we have to find the syntax for this ourselves???????? Jesus Freaking Kennedy
+        #Source:
+        #https://github.com/sintefmath/Splipy/blob/d2b6dea5055e5c8435b729969aceb0bf170024b2/doc/Tutorial/Factory%20methods.ipynb
+        plt.plot(X[:,:,0], X[:,:,1], '-', color="red")
+        plt.plot(X[:,:,0], X[:,:,1].T, '-', color="blue")
+        plt.axis('equal')
+        plt.show()
 
-	
-	x = []
-	y = []
-	z = []
-	for vec in my_obj[0]:
-		x.append(vec[0])
-		y.append(vec[0])
-		z.append(0)
-
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	ax.plot_surface(x,y,z)
-	plt.show()
-	plt.figure()
-	plt.plot(y,x)
-	plt.show()
 
 Area_Plot()
