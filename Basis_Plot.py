7faa6e3f770aa52fadb7c5caec34acc23536e0b2
Erik Olsvik Dengerud <eriko1306@gmail.com>
2017-11-21 20:05:59 +0100

    removed import sys

--- Basis_Plot.py
+++ Basis_Plot.py
@@ -2,7 +2,6 @@
 import numpy as np
 import splipy as spl
 import matplotlib.pyplot as plt
-import sys
 ################################################################################
 
 def Basis_Plot():
