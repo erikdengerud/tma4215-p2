8af74cb77c0fcdacb4e79a04811c68f71b4ce53b
William <williambuvik@gmail.com>
2017-11-22 19:30:06 +0100

    Made it into a function, Area_Integral()

--- Area_Integral.py
+++ Area_Integral.py
@@ -11,37 +11,40 @@ import splipy as spl
 from splipy.io import G2
 import splipy.surface_factory as spf
 from Spline_Quadrature import Spline_Quadrature, Assembly, Prepare_Data
+import time
 ################################################################################
+def Area_Integral():
+    with G2("Area.g2") as file:
+        surface = file.read()[0]
+    #2D
+    N = 25
+    u = np.linspace(surface.start('u'), surface.end('u'), N)
+    v = np.linspace(surface.start('v'), surface.end('v'), N)
+    x = surface(u, v)
+    
+    tau_u, tau_v = surface.knots(0,True), surface.knots(1,True)
+    
+    p_u, p_v = surface.order()
+    
+    p_u += -1
+    p_v += -1
+    
+    W_u, X_u, ehh = Spline_Quadrature(tau_u,p_u)
+    W_v, X_v, ehh = Spline_Quadrature(tau_v,p_v)
+    
+    #du = surface.derivative(X_u, X_v, d=(1,0))
+    #dv = surface.derivative(X_u, X_v, d=(0,1))
+    
+    print('Area calculated by splipy : ',surface.area())
+    
+    summen = 0
+    
+    for i in np.arange(len(X_u)):
+        for j in np.arange(len(X_v)):
+            du = surface.derivative(X_u[i], X_v[j], d=(1,0))
+            dv = surface.derivative(X_u[i], X_v[j], d=(0,1))
+            summen += np.abs(W_u[i]*W_v[j]*np.cross(du,dv))
+            
+    print('Area calculated from spline quadrature and Jacoabian integral/sum : ',summen)
 
-with G2("Area.g2") as file:
-    surface = file.read()[0]
-#2D
-N = 25
-u = np.linspace(surface.start('u'), surface.end('u'), N)
-v = np.linspace(surface.start('v'), surface.end('v'), N)
-x = surface(u, v)
-
-tau_u, tau_v = surface.knots(0,True), surface.knots(1,True)
-
-p_u, p_v = surface.order()
-
-p_u += -1
-p_v += -1
-
-W_u, X_u, ehh = Spline_Quadrature(tau_u,p_u)
-W_v, X_v, ehh = Spline_Quadrature(tau_v,p_v)
-
-#du = surface.derivative(X_u, X_v, d=(1,0))
-#dv = surface.derivative(X_u, X_v, d=(0,1))
-
-print('Area calculated from splipy : ',surface.area())
-
-summen = 0
-
-for i in np.arange(len(X_u)):
-    for j in np.arange(len(X_v)):
-        du = surface.derivative(X_u[i], X_v[j], d=(1,0))
-        dv = surface.derivative(X_u[i], X_v[j], d=(0,1))
-        summen += np.abs(W_u[i]*W_v[j]*np.cross(du,dv))
-        
-print('Area calculated from spline quadrature and Jacoabian integral/sum : ',summen)
+Area_Integral()
